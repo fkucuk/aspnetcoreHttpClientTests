4423bd5ebf8cbabd48f148ee306bd80e7f3f88ef
FATIH KUCUK <FATIH.KUCUK@lcwaikiki.com>
2021-04-23 10:01:03 +0300

    async versions added to framework side

--- NetFramework4_6BFF/Controllers/MobileController.cs
+++ NetFramework4_6BFF/Controllers/MobileController.cs
@@ -3,6 +3,8 @@ using RestSharp;
 using System;
 using System.Configuration;
 using System.Net;
+using System.Net.Http;
+using System.Net.Http.Headers;
 using System.Threading.Tasks;
 using System.Web.Http;
 
@@ -12,11 +14,15 @@ namespace NetFrameworkBFF.Controllers
     public class MobileController : ApiController
     {
 
+        static HttpClient _client = new HttpClient { BaseAddress = new Uri(ConfigurationManager.AppSettings["OldServiceUrl"].ToString()) };
 
         private readonly string oldServiceUrl;
         public MobileController()
         {
             this.oldServiceUrl = ConfigurationManager.AppSettings["OldServiceUrl"].ToString();
+            _client.DefaultRequestHeaders.Accept.Clear();
+            _client.DefaultRequestHeaders.Accept.Add(
+                new MediaTypeWithQualityHeaderValue("application/json"));
         }
 
         [HttpPost]
@@ -83,6 +89,40 @@ namespace NetFrameworkBFF.Controllers
 
 
             return Content(HttpStatusCode.BadRequest, "hata");
+        }
+
+
+        [HttpPost]
+        [Route("mobile/v3/getlabelinfo")]
+        public async Task<IHttpActionResult> Post_v3([FromBody] object requestObj)
+        {
+            try
+            {
+
+
+                string body = JsonConvert.SerializeObject(requestObj);
+
+
+                HttpResponseMessage response = await _client.PostAsJsonAsync("getlabelinfo", requestObj);
+
+                response.EnsureSuccessStatusCode();
+
+
+                var content = await response.Content.ReadAsStringAsync();
+
+                var result = JsonConvert.DeserializeObject(content);
+
+                return Content(HttpStatusCode.OK, result);
+
+
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e);
+                return Content(HttpStatusCode.NotAcceptable, "deneme");
+            }
+
+
         }
     }
 }
---
+++ NetFramework4_6BFF/Models/GetLabelInfoResponse.cs
@@ -0,0 +1,115 @@
+﻿using System;
+
+namespace NetFramework4_6BFF.Models
+{
+    public class GetLabelInfoResponse
+    {
+
+        public LabelDetail LabelDetailField { get; set; }
+
+        public PriceDetail PriceDetailField { get; set; }
+
+        public ProductDetail ProductDetailField { get; set; }
+
+        public StockDetail StockDetailField { get; set; }
+        public TransferDetail TransferDetailField { get; set; }
+
+
+
+    }
+
+    public class LabelDetail
+    {
+
+        public string AtamaTarihField { get; set; }
+
+        public string BuyukBeyazEtiketZplField { get; set; }
+
+        public System.Nullable<int> EtiketTipField { get; set; }
+
+        public bool FirsatUrunuMuField { get; set; }
+
+        public System.Nullable<System.DateTime> FiyatDegisimTarihField { get; set; }
+
+        public string IcBarkodEtiketZplField { get; set; }
+
+        public string IlkFiyatDegisimEtiketZplField { get; set; }
+
+        public string IndirimliEtiketZplField { get; set; }
+
+        public System.Nullable<decimal> IskontoOranField { get; set; }
+
+        public string KontrolBarkodField { get; set; }
+
+        public bool TaksitEngelliMiField { get; set; }
+    }
+
+    public class PriceDetail
+    {
+        public System.Nullable<double> IlkPesinFiyatField { get; set; }
+        public System.Nullable<double> IlkTaksitliFiyatField { get; set; }
+        public System.Nullable<double> PesinFiyatField { get; set; }
+        public System.Nullable<double> TaksitliFiyatField { get; set; }
+    }
+
+    public class ProductDetail
+    {
+
+        public string AdField { get; set; }
+
+        public string BarkodField { get; set; }
+
+        public string BedenField { get; set; }
+
+        public int BedenSiraField { get; set; }
+
+        public string LineTanimField { get; set; }
+
+        public string OzelKodField { get; set; }
+
+        public string RenkField { get; set; }
+
+        public string RenkHexKodField { get; set; }
+
+        public string RenkKodField { get; set; }
+
+        public int UrunIdField { get; set; }
+
+        public string UrunKlasmanField { get; set; }
+
+        public bool YerliMaliMiField { get; set; }
+    }
+
+    public class StockDetail
+    {
+
+        public Nullable<int> DepoStokField { get; set; }
+
+        public Nullable<int> OptionStokField { get; set; }
+
+        public Nullable<int> ReyonStokField { get; set; }
+    }
+
+    public class TransferDetail
+    {
+
+        public TransferEntity[] TransferListField { get; set; }
+
+    }
+
+    public class TransferEntity 
+    {
+
+        public string BarkodField { get; set; }
+
+        public string BedenField { get; set; }
+
+        public string BoyField { get; set; }
+
+        public string ToDepoField { get; set; }
+
+        public System.Nullable<int> TransferMiktarField { get; set; }
+
+        public string TransferTipField { get; set; }
+    }
+    }
